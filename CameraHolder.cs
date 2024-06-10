838d75b7a7844790c47a032399260f6dcefc8de6
Deleted_Account2131 <spectralcodee@gmail.com>
2024-06-10 14:03:24 +0200

    Create CameraHolder.cs

---
+++ CameraHolder.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class CameraHolder : MonoBehaviour
+{
+    
+    public Transform cameraPosition;
+
+    void Update()
+    {
+        transform.position = cameraPosition.position;
+    }
+}
